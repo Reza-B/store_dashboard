8cdda2a1710829fd7325d62c6ec8cb9c9dde7312
Reza Buzarjemehri <dev.reza21@gmail.com>
2024-09-17 03:11:19 +0330

    login ui

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,59 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useContext, useState } from "react";
 import AuthContext from "../context/AuthContext";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import FormLabel from "@mui/material/FormLabel";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import MuiCard from "@mui/material/Card";
+import { styled } from "@mui/material/styles";
+import AppTheme from "../theme/AppTheme";
 
-const Login: React.FC = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const navigate = useNavigate();
-  const context = React.useContext(AuthContext);
+const Card = styled(MuiCard)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  alignSelf: "center",
+  width: "100%",
+  padding: theme.spacing(4),
+  gap: theme.spacing(2),
+  margin: "auto",
+  [theme.breakpoints.up("sm")]: {
+    maxWidth: "450px",
+  },
+  boxShadow: "hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px",
+  ...theme.applyStyles("dark", {
+    boxShadow: "hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px",
+  }),
+}));
+
+const SignInContainer = styled(Stack)(({ theme }) => ({
+  padding: 20,
+  marginTop: "10vh",
+  "&::before": {
+    content: '""',
+    display: "block",
+    position: "absolute",
+    zIndex: -1,
+    inset: 0,
+    backgroundImage: "radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))",
+    backgroundRepeat: "no-repeat",
+    ...theme.applyStyles("dark", {
+      backgroundImage: "radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))",
+    }),
+  },
+}));
+
+export default function Login() {
+  const [emailError, setEmailError] = useState(false);
+  const [emailErrorMessage, setEmailErrorMessage] = useState("");
+  const [passwordError, setPasswordError] = useState(false);
+  const [passwordErrorMessage, setPasswordErrorMessage] = useState("");
+
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const context = useContext(AuthContext);
 
   if (!context) {
     return <div>Error: AuthContext is not available</div>;
@@ -14,29 +61,109 @@ const Login: React.FC = () => {
 
   const { login } = context;
 
-  const handleLogin = () => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
     const token = "your_jwt_token";
     login(token);
   };
 
-  return (
-    <div>
-      <h1>Login Page</h1>
-      <input
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleLogin}>Login</button>
-    </div>
-  );
-};
+  const validateInputs = () => {
+    let isValid = true;
 
-export default Login;
+    if (!email || !/\S+@\S+\.\S+/.test(email)) {
+      setEmailError(true);
+      setEmailErrorMessage("لطفا بک ایمیل معتبر وارد کنید");
+      isValid = false;
+    } else {
+      setEmailError(false);
+      setEmailErrorMessage("");
+    }
+
+    if (!password || password.length < 8) {
+      setPasswordError(true);
+      setPasswordErrorMessage("کلمه عبور حداقل 8 حرف است.");
+      isValid = false;
+    } else {
+      setPasswordError(false);
+      setPasswordErrorMessage("");
+    }
+
+    return isValid;
+  };
+
+  return (
+    <AppTheme>
+      <SignInContainer direction="column" justifyContent="space-between">
+        <Card variant="outlined">
+          <Typography
+            component="h1"
+            variant="h4"
+            textAlign="center"
+            sx={{ width: "100%", fontSize: "clamp(2rem, 10vw, 2.15rem)" }}>
+            {"ورود"}
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              width: "100%",
+              gap: 2,
+            }}>
+            <FormLabel htmlFor="email">{"ایمیل"}</FormLabel>
+            <TextField
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              error={emailError}
+              helperText={emailErrorMessage}
+              type="email"
+              placeholder="your@email.com"
+              autoComplete="email"
+              autoFocus
+              required
+              fullWidth
+              variant="outlined"
+              color={emailError ? "error" : "primary"}
+              sx={{ ariaLabel: "email", direction: "ltr" }}
+            />
+            <FormLabel htmlFor="password">{"کلمه عبور"}</FormLabel>
+            <TextField
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              error={passwordError}
+              helperText={passwordErrorMessage}
+              placeholder="••••••"
+              type="password"
+              autoComplete="current-password"
+              autoFocus
+              required
+              fullWidth
+              variant="outlined"
+              color={passwordError ? "error" : "primary"}
+              sx={{
+                direction: "ltr",
+              }}
+            />
+            <Button
+              variant="contained"
+              onClick={validateInputs}
+              sx={{
+                mt: 8,
+              }}>
+              <Typography sx={{ textAlign: "center" }}>
+                <Typography>{"ورود"}</Typography>
+              </Typography>
+            </Button>
+          </Box>
+        </Card>
+      </SignInContainer>
+    </AppTheme>
+  );
+}
